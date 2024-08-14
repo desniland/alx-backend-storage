16f9d379884d60e1fc95e794b006bcb54957511b
desniland <albertdesmond26@gmail.com>
2024-08-14 15:13:16 +0300

    Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -35,4 +35,5 @@ def get_page(url: str) -> str:
     '''Returns the content of a URL after caching the request's response,
     and tracking the request.
     '''
+
     return requests.get(url).text
